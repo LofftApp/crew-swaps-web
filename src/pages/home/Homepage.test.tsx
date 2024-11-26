ecac64ca2b32bc4fe1d0042cf16a4a04f16ef3ea
Jose Copeti <jrcopeti@gmail.com>
2024-11-26 21:07:41 +0100

    setup for tests and first test Homepage

---
+++ src/pages/home/Homepage.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import Homepage from "./Homepage";
+
+describe("Homepage Component", () => {
+  it("renders 'This is homepage'", () => {
+    render(<Homepage />);
+    const textElement = screen.getByText("This is homepage");
+    expect(textElement).toBeInTheDocument(); // Use .toBeInTheDocument() for semantic clarity
+  });
+});
