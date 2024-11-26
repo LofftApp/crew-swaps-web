006de271402bbef5b6aeebfd06760b726b6a361c
Jose Copeti <jrcopeti@gmail.com>
2024-11-27 00:12:31 +0100

    installed DaysiUI and defined color palletes

--- src/components/BgImage.tsx
+++ src/components/BgImage.tsx
@@ -5,7 +5,7 @@ function BgImage({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        `absolute left-0 z-0 h-52 w-52 flex-col bg-center bg-no-repeat`,
+        `absolute z-0 h-52 w-52 flex-col bg-center bg-no-repeat`,
         className,
       )}
       data-testid="bg-image"
--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -2,11 +2,19 @@ import BgImage from "../../components/BgImage";
 
 function Homepage() {
   return (
-    <div className="h-screen">
-      <BgImage className="top-72" />
-      <BgImage className="top-0 left-96" />
+    <div className="bg-base-50 relative h-screen overflow-hidden">
+      {/* Left Image */}
+      <BgImage className="left-0 top-0 -translate-x-[30%] translate-y-[50%] transform md:-translate-x-[10%]" />
 
-      <h1 className="text-center text-3xl font-bold">This is homepage</h1>
+      {/* Right Image */}
+      <BgImage className="right-0 top-0 translate-x-1/3 translate-y-[150%] transform md:translate-x-[10%]" />
+
+      <div className="relative z-10 flex h-full items-center justify-center">
+        <h1 className="text-secondary text-center text-3xl font-bold">
+          This is homepage
+        </h1>
+        <button className="btn btn-primary btn-outline">Try for Free</button>
+      </div>
     </div>
   );
 }
