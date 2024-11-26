836d9e436255a999246100db355bd7a6b4cd6538
Jose Copeti <jrcopeti@gmail.com>
2024-11-26 22:09:02 +0100

    svg in homepage

--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -1,5 +1,14 @@
+import bgHomepage from "../../assets/svg/bg-homepage.svg";
+
 function Homepage() {
-  return <div>This is homepage</div>;
+  return (
+    <div
+      className="flex h-screen flex-col items-center justify-center"
+      style={{ backgroundImage: `url(${bgHomepage})` }}
+    >
+      <h1 className="text-4xl font-bold text-gray-800">This is homepage</h1>
+    </div>
+  );
 }
 
 export default Homepage;
