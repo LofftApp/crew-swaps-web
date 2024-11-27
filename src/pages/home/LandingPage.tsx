3e9671743d28df2479e903b12d939b204de22a4b
Jose Copeti <jrcopeti@gmail.com>
2024-11-27 18:43:27 +0100

    trusted badge

--- src/pages/home/LandingPage.tsx
+++ src/pages/home/LandingPage.tsx
@@ -11,8 +11,8 @@ function LandingPage() {
       <BgImage className="absolute left-0 top-0 -translate-x-1/3 transform md:-translate-x-1/4" />
       {/* Right Image */}
 
-      <BgImage className="xs:-translate-y-[100%] bottom-0 right-0 -translate-y-[60%] translate-x-1/3 rotate-180 transform md:translate-x-1/4" />
-      <div className="xs:p-8 xs:-mt-10 flex flex-col items-center justify-center gap-4 p-12">
+      <BgImage className="xs:-translate-y-[102%] bottom-0 right-0 -translate-y-[65%] translate-x-1/3 rotate-180 transform md:translate-x-1/4" />
+      <div className="xs:p-8 xs:mt-2 flex flex-col items-center justify-center gap-4 p-12">
         <h1 className="text-center text-4xl font-semibold capitalize leading-[3.5rem]">
           Swap shifts with your team members fast and easy
         </h1>
@@ -25,11 +25,23 @@ function LandingPage() {
           <Feature name="Fast" />
           <Feature name="Secure" />
         </div>
+        <div className="ml-2 flex w-[90%] xs:w-[75%] items-center gap-2 rounded-full bg-base-300 p-2 text-sm justify-center">
+          <div className="badge badge-primary text-sm font-semibold">
+            Trusted
+          </div>
+          <p className=''>
+
+            by the leading airlines ✨
+            </p>
+        </div>
+
         <div className="mt-2 flex items-center gap-4">
           <button className="btn btn-primary">Get Started</button>
           <button className="btn btn-outline btn-primary">Learn More</button>
         </div>
       </div>
+      <div></div>
+
       <FooterLandingPage />
     </div>
   );
