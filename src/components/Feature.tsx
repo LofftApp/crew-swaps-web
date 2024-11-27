06b11b169661e076e82a16993a61739b93548128
Jose Copeti <jrcopeti@gmail.com>
2024-11-27 22:42:34 +0100

    landing page repsonsive for bigger screens

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -2,9 +2,9 @@ import { CircleCheckBig } from "lucide-react";
 
 function Feature({ name = "easy" }: { name: string }) {
   return (
-    <div className='flex gap-2 items-center'>
-      <CircleCheckBig className='h-6 w-6 text-info' />
-      <p className='text-base font-medium '>{name}</p>
+    <div className="flex items-center gap-2">
+      <CircleCheckBig className="h-6 w-6 text-info md:h-8 md:w-8 xl:h-10 xl:w-10" />
+      <p className="text-base font-medium md:text-lg xl:text-xl">{name}</p>
     </div>
   );
 }
--- src/pages/home/LandingPage.tsx
+++ src/pages/home/LandingPage.tsx
@@ -12,27 +12,24 @@ function LandingPage() {
       {/* Right Image */}
 
       <BgImage className="xs:-translate-y-[102%] bottom-0 right-0 -translate-y-[65%] translate-x-1/3 rotate-180 transform md:translate-x-1/4" />
-      <div className="xs:p-8 xs:mt-2 flex flex-col items-center justify-center gap-4 p-12">
-        <h1 className="text-center text-4xl font-semibold capitalize leading-[3.5rem]">
+      <div className="xs:p-8 xs:mt-2 flex flex-col items-center justify-center gap-4 p-12 md:gap-6 md:p-12 xl:gap-8 xl:p-20">
+        <h1 className="text-center text-4xl font-semibold capitalize leading-[3.5rem] md:text-5xl md:leading-[4rem] xl:text-6xl xl:leading-[4.5rem] 2xl:text-7xl 2xl:leading-[5rem]">
           Swap shifts with your team members fast and easy
         </h1>
-        <p className="text-center text-lg text-base-content/75">
+        <p className="text-center text-lg text-base-content/75 md:text-2xl xl:text-3xl 2xl:text-4xl">
           Whenever you need to swap shifts, we've got you covered. Just sign in
           and start swapping.
         </p>
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-6 xl:gap-14">
           <Feature name="Easy" />
           <Feature name="Fast" />
           <Feature name="Secure" />
         </div>
-        <div className="ml-2 flex w-[90%] xs:w-[75%] items-center gap-2 rounded-full bg-base-300 p-2 text-sm justify-center">
+        <div className="xs:w-[80%] ml-2 flex w-[90%] items-center justify-center gap-2 rounded-full bg-base-300 p-2 text-sm sm:max-w-[50%] md:max-w-[55%] md:text-base lg:max-w-[35%] xl:max-w-[30%] xl:text-lg 2xl:text-xl">
           <div className="badge badge-primary text-sm font-semibold">
             Trusted
           </div>
-          <p className=''>
-
-            by the leading airlines ✨
-            </p>
+          <p className="">by the leading airlines crews ✨</p>
         </div>
 
         <div className="mt-2 flex items-center gap-4">
