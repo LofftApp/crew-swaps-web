d4183ba88efed9b4007262cf7ccd19e3db4824f0
Jose Copeti <jrcopeti@gmail.com>
2024-11-30 11:49:58 +0100

    layout sign in page

--- src/components/ContainerWithBgImage.tsx
+++ src/components/ContainerWithBgImage.tsx
@@ -1,22 +1,23 @@
 import BgImage from "./BgImage";
+type ContainerWithBgImageProps = {
+  className?: string;
+  children: React.ReactNode;
+  navbar?: React.ReactNode;
+};
 
 function ContainerWithBgImage({
   className,
   children,
   navbar,
-}: {
-  className?: string;
-  children: React.ReactNode;
-  navbar?: React.ReactNode;
-}) {
+}: ContainerWithBgImageProps) {
   return (
     <div className="relative mx-auto flex min-h-screen flex-col items-center justify-between overflow-hidden">
       {navbar}
       {/* Left Image */}
       <BgImage className="absolute left-0 top-0 -translate-x-1/3 transform md:-translate-x-1/4" />
       {/* Right Image */}
-      <BgImage className="bottom-0 right-0 -translate-y-[65%] translate-x-1/3 rotate-180 transform xs:-translate-y-[72%] md:-translate-y-[76%] md:translate-x-1/4" />
-      <div className={className}>{children}</div>
+      <BgImage className="bottom-0 right-0 -translate-y-[60%] translate-x-1/3 rotate-180 transform xs:-translate-y-[72%] md:-translate-y-[76%] md:translate-x-1/4" />
+      {children}
     </div>
   );
 }
--- src/pages/signIn/SignInPage.tsx
+++ src/pages/signIn/SignInPage.tsx
@@ -1,13 +1,54 @@
 import BgImage from "../../components/BgImage";
+import ContainerWithBgImage from "../../components/ContainerWithBgImage";
+import Feature from "../../components/Feature";
+import FooterLandingPage from "../../components/FooterLandingPage";
+import TrustedBadge from "../../components/TrustedBadge";
+import NavbarHomepage from "../../components/NavbarLandingPage";
+import { Link } from "react-router";
 
 function SignInPage() {
   return (
-    <div className="relative mx-auto flex min-h-screen flex-col items-center justify-between overflow-hidden">
-      {/* Left Image */}
-      <BgImage className="absolute left-0 top-0 -translate-x-1/3 transform md:-translate-x-1/4" />
-      {/* Right Image */}
-      <BgImage className="bottom-0 right-0 -translate-y-[65%] translate-x-1/3 rotate-180 transform xs:-translate-y-[72%] md:-translate-y-[76%] md:translate-x-1/4" />
-    </div>
+    <ContainerWithBgImage navbar={<NavbarHomepage />}>
+      <div className="flex max-w-screen-2xl flex-col items-center justify-center gap-4 p-12 xs:mt-[40%] xs:p-8 md:gap-6 md:p-12 lg:gap-8 xl:p-20">
+        <h1 className="text-center text-4xl font-semibold capitalize leading-[3.5rem] md:text-5xl md:leading-[4rem] lg:text-6xl lg:leading-[4.5rem] 2xl:text-7xl 2xl:leading-[5rem]">
+          Sign in to your account
+        </h1>
+        <p className="text-center text-lg text-base-content/80 md:text-2xl lg:text-3xl 2xl:text-4xl">
+          Welcome back!
+        </p>
+        <div className="flex flex-col gap-8">
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+              <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+            </svg>
+            <input type="text" className="grow" placeholder="Email" />
+          </label>
+
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input type="password" className="grow" value="password" />
+          </label>
+        </div>
+      </div>
+        <FooterLandingPage />
+    </ContainerWithBgImage>
   );
 }
 
