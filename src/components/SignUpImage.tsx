da2b7457b70d6a0abf66e38896574a6385f444a9
Jose Copeti <jrcopeti@gmail.com>
2024-12-01 23:33:33 +0100

    changed signUp image

--- src/components/SignUpImage.tsx
+++ src/components/SignUpImage.tsx
@@ -1,10 +1,10 @@
-import image2 from "../assets/illustration/illustration-2.png";
+import image3 from "../assets/illustration/illustration-3.png";
 
 function SignUpImage() {
   return (
     <div className="hidden p-0 text-center text-base text-base-content/70 xs:text-lg md:p-12 lg:block xl:p-12">
       <img
-        src={image2}
+        src={image3}
         alt="signup-image"
         data-testid="signup-image"
         className="h-[600px] w-[500px] object-cover object-center 2xl:h-[700px] 2xl:w-[600px]"
